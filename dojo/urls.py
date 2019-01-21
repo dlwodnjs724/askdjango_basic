6c3e9bf13f851368ca64d320c587a157a84b80a3
dlwodnjs724 <dlwodnjs724@naver.com>
2019-01-21 17:07:20 +0900

    4강: 함수 기반 뷰 (FBV) 의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # how? path('sum/<str:numbers>/', views.mysum)
     path('hello/<str:name>/<int:age>/', views.hello),
     # re_path(r'hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello)
+    path('list1/', views.post_list1),
+    path('list2/', views.post_list2),
+    path('list3/', views.post_list3),
+    path('excel/', views.execl_download)
 ]
