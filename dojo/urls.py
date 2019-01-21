3319ecb45d1e9b46effa7f86e3c85b87250f760a
dlwodnjs724 <dlwodnjs724@naver.com>
2019-01-21 15:52:47 +0900

    3강: dojo.views.hello 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     re_path(r'sum/(?P<numbers>[\d/]+)/$', views.mysum),
     # how? path('sum/<str:numbers>/', views.mysum)
+    path('hello/<str:name>/<int:age>/', views.hello),
+    # re_path(r'hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello)
 ]
