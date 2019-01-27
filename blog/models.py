6b923c622120373fb3e2dc06d94163b57b10f932
dlwodnjs724 <dlwodnjs724@naver.com>
2019-01-27 23:02:55 +0900

    11강: blog.models.Tag 모델 생성/admin 등록

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
                               validators=[lnglat_validator],
                               help_text='경도/위도 포맷으로 입력')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    tag_set = models.ManyToManyField('Tag')                     # 문자열로 모델명을 써줌
     created_at = models.DateTimeField(auto_now_add=True)        # auto_now_add=True: 최초로 생성될 때 시간 자동 저장
     updated_at = models.DateTimeField(auto_now=True)            # auto_now=True: 갱신될 때마다 시간 자동 저장
 
@@ -39,3 +40,10 @@ class Comment(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
