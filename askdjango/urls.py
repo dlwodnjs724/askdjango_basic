bb127c0b211269e5252c57d0a9be13d6ddb8d41a
dlwodnjs724 <dlwodnjs724@naver.com>
2019-01-27 23:24:54 +0900

    11강: 새로이 accounts 앱을 생성/등록

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('dojo/', include('dojo.urls')),
 ]
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 import re
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ValidationError
 
