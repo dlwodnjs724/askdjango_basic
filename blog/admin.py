12967d65d8c2985f71e65b494a3831b1eb825b28
dlwodnjs724 <dlwodnjs724@naver.com>
2019-01-27 22:33:07 +0900

    11강: blog.models.Comment 모델 생성/admin 동록

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -24,3 +24,8 @@ class PostAdmin(admin.ModelAdmin):
 
 
 # admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
