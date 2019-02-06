fa0e889b06cb5c212a88ab83d44ded2648ea2d87
dlwodnjs724 <dlwodnjs724@naver.com>
2019-02-06 20:10:51 +0900

    31강: CBV - CreateView/UpdateView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('<int:pk>/', views_cbv.post_detail, name='post_detail'),
 
     path('new/', views.post_new, name='post_new'),
-    path('<int:id>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
 
-    path('cbv/new/', views_cbv.post_new)
+    path('cbv/new/', views_cbv.post_new),
+    path('cbv/<int:pk>/edit/', views_cbv.post_edit),
 ]
